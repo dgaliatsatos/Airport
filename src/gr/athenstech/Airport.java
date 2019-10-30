652788cb10dda4cd02c94d586047dd8b723944f3
dgaliatsatos <dgaliatsatos@athtech.gr>
2019-10-30 13:25:49 +0200

    Initial Commit 30/10 1:19pm

--- src/gr/athenstech/Airport.java
+++ src/gr/athenstech/Airport.java
@@ -6,5 +6,8 @@ public class Airport {
 	Flights f1= new Flights();
 	f1.setArAirport("Athens");
 	f1.setDepAirport("California");
+	System.out.println(f1);
+	System.out.println("from"+f1.getArAirport());
+	System.out.println(f1.getDepAirport());
     }
 }
